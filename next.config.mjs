d7b6c2f6c1c749942d5e688ae11e37d2d21a6a34
Sarvesh Upadhyay <89679894+sarvesh-upadhyay21@users.noreply.github.com>
2025-11-21 12:17:19 +0530

    gdfgdfg

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,22 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
+const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
 const nextConfig = {
-  // remove basePath/assetPrefix if you serve at domain root on Vercel
-  // basePath: isProd ? "/E-learning" : "",
-  // assetPrefix: isProd ? "/E-learning/" : "",
-  images: {
-    unoptimized: true,
-  },
+  basePath: isGhPages ? "/E-learning" : "",
+  assetPrefix: isGhPages ? "/E-learning/" : "",
+  ...(isGhPages ? { output: "export" } : {}),
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig = {
+//   // remove basePath/assetPrefix if you serve at domain root on Vercel
+//   // basePath: isProd ? "/E-learning" : "",
+//   // assetPrefix: isProd ? "/E-learning/" : "",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
