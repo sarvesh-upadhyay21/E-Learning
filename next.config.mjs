bef4442e74fa627484df53256d2455c47b39539a
Sarvesh Upadhyay <89679894+sarvesh-upadhyay21@users.noreply.github.com>
2025-11-21 12:06:06 +0530

    gsgsd

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
+const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
 
 const nextConfig = {
   basePath: isProd ? "/E-learning" : "",
   assetPrefix: isProd ? "/E-learning/" : "",
-  output: "export",
+  ...(isGhPages ? { output: "export" } : {}), // only use export when DEPLOY_TARGET=gh-pages
   images: {
     unoptimized: true,
   },
